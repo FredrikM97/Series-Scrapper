bf89d155bf194d769a22a7f868f43d9e53b516b3
FredrikM97 <fredrik9779@gmail.com>
2020-02-09 21:47:34 +0100

    Cleanup in main

--- main.go
+++ main.go
@@ -1,129 +1,154 @@
 package main
+
 import (
-	"fmt"
-	"os"
-	"encoding/json"
-	"io/ioutil"
-	"github.com/mitchellh/mapstructure"
 	"flag"
-	//"net/http"
-	//"strings"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
 )
 
-// Global variables
-var DATABASE = "database/"
+/*
+Structs
+*/
+type Commands struct {
+	name, seasonal, url                string
+	genre                              string
+	score, rank, episodes, info, aired bool
+}
 
-// Structs
 type Sites struct {
-    Sites 	[]Site 		`json:"sites"`
+	Sites []Site `json:"sites"`
 }
 
 type Site struct {
-	Name	string		`json:"name"`
-	Url   	string 		`json:"url"`
-	Genre  	[]string 	`json:"genre"`
+	Name   string            `json:"name"`
+	Url    string            `json:"url"`
+	Search string            `json:"search"`
+	Genre  map[string]string `json:"genre"`
 }
 
-var (
-	name, seasonal, url					string 
-	score, rank, episodes,info, aired	bool
+// Convert map value to function, return bool
+type Map2func func(string) bool
 
-)
-
-
+/*
+Global var
+*/
+var DATABASE = "database/"
+var command_map = new(Commands)
+var sites Sites
+var available_sites = map[string]Map2func{
+	"myanimelist": search_MAL,
+}
 
 func main() {
 	/*
-	Init system from here
+		Init system from here
 	*/
 	fmt.Println("Starting system!")
-	var sites Sites
+	setFlags()
 
-	data := json_handler(DATABASE + "sites.json", sites)
+	data := json_handler(DATABASE+"sites.json", sites)
 	mapstructure.Decode(data, &sites)
 
-	fmt.Println("\nData: " +  fmt.Sprint(sites))
-	setFlags()
-}
+	print_db(sites)
 
-func setFlags() {	
+	// Check input
+	if command_map.name != "" {
+		bool_check := checkParams()
+		if bool_check {
+			site := sites.Sites[0] // TODO: Change so we check all availbile sites
+			success := Search(site)
+
+			if success {
+
+			}
+
+		}
+	} else if command_map.seasonal != "" {
+
+	}
+}
+func checkParams() bool {
 	/*
-	Handles params for shell, TODO: Fix so commands stay in database instead (easier to organize)
+		Check if any parameters is given, if not, return boolcheck false
+		@Params: Commands
+
+		@Return: bool
 	*/
-	flag.StringVar(&name, "name", "", "Write a name of series")
-	flag.StringVar(&seasonal, "seasonal", "", "Format: <SEASON> <YEAR>, blank gives the current season")
-	flag.BoolVar(&score, "score", false, "Get score of series")
-	flag.BoolVar(&rank, "rank", false, "Get rank of series")
-	flag.BoolVar(&episodes, "episodes", false, "Get number of episodes")
-	flag.BoolVar(&info, "info", false, "Get information of series")
-	flag.BoolVar(&aired, "aired", false, "Get aired date of series")
-        
+	r := reflect.ValueOf(command_map).Elem()
+	for i := 0; i < r.NumField(); i++ {
+
+		//Get value of param
+		f := r.Field(i)
+
+		if f.Kind() == reflect.Bool && reflect.ValueOf(true).Bool() == f.Bool() {
+			return true
+		}
+	}
+	return false
+}
+func setFlags() {
+	/*
+		Handles params for shell, TODO: Fix so commands stay in database instead (easier to organize)
+	*/
+
+	flag.StringVar(&command_map.name, "name", "", "Write a name of series")
+	flag.StringVar(&command_map.seasonal, "seasonal", "", "Format: <SEASON> <YEAR>, blank gives the current season")
+	flag.BoolVar(&command_map.score, "score", false, "Get score of series")
+	flag.BoolVar(&command_map.rank, "rank", false, "Get rank of series")
+	flag.BoolVar(&command_map.episodes, "episodes", false, "Get number of episodes")
+	flag.BoolVar(&command_map.info, "info", false, "Get information of series")
+	flag.BoolVar(&command_map.aired, "aired", false, "Get aired date of series")
+
 	flag.Parse()
 }
 
-func json_handler(db string, data interface{}) interface{} {
+func print_db(sites Sites) {
 	/*
-	Import data from database
+		Print info from the database
 	*/
-	
-	db_file, err := os.Open(db)
-
-	if err != nil {
-		fmt.Print(err)
-	}
-	fmt.Println("Successfully opened " + db)
-
-	defer db_file.Close()
-	
-	byteValue, _ := ioutil.ReadAll(db_file)
-	
-	json.Unmarshal(byteValue, &data)
-
-	fmt.Println("\nData: " +  fmt.Sprint(data))
-	return data
-}
-func print_db(sites Sites){
-	/*
-	Print info from the database
-	*/
+	fmt.Println("Sites:")
 	for i := 0; i < len(sites.Sites); i++ {
 		fmt.Println("Name: " + sites.Sites[i].Name)
 		fmt.Println("Url: " + sites.Sites[i].Url)
 		fmt.Println("Genres: " + fmt.Sprint(sites.Sites[i].Genre))
 	}
 }
-func getContent(url string) (string, bool) {
-	/*
-	Get content on page
-	@Params
-	* url: url to page
 
-	@return
-	* string: info from site
-	* bool: if match found or not
+func getSeasonal() {
+
+}
+func getTopTen() {
+
+}
+
+func getDetails() {
+
+}
+
+func Search(site Site) bool {
+	/*
+		Search on site based on parameters from shell
+		TODO: Should check if parameters is given or not too
 	*/
-	/*
-	data_resp, err := http.GET(url)
+	// TODO: Handle multiple different sites
 
-	if err != nil {
-		fmt.Println("Error: Could not fetch page!")
-		return "", true
+	if _, ok := available_sites[site.Name]; !ok {
+		return false
 	}
-	defer data_resp.Close()
-	*/
-	return "",false
 
-}
+	var genre string
+	if val, ok := site.Genre[command_map.genre]; ok {
+		genre = site.Genre[val]
+	} else {
+		genre = site.Genre["0"]
+	}
+	r := strings.NewReplacer("*genre*", genre)
+	search := r.Replace(site.Search)
 
-func getSeasonal(){
+	search_url := site.Url + search + strings.ToLower(command_map.name)
+	return available_sites[site.Name](search_url)
 
-}
-func getTopTen(){
-
-}
-
-func Search() {
-	/*
-	Search on site based on parameters from shell
-	*/
 }
